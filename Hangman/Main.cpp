c5e7a0ec27ff5a38752f41877d7f3e9f37e03f27
jackster1302 <52420708+jackster1302@users.noreply.github.com>
2020-10-10 17:22:24 +0100

    Re did the program but used Sets insted of Vectors

--- Hangman/Main.cpp
+++ Hangman/Main.cpp
@@ -1,41 +1,274 @@
-#include <IOstream> //for using cin anc cout
-#include <string>  // using this so we have acces to the String object for the words used in the game
-#include <conio.h> // using this for _getch()
-#include <ctime>  //so we can get a random number 
-#include <vector> //using vectors to hold the players guess
- 
+#include<iostream>
+#include<vector>
+#include <set>
+#include <conio.h>
+#include <string>
+#include <ctype.h>
+#include <stdio.h>
+#include <Windows.h>
+#include <ctime>
 
-// played the intro to the user and allows them to pick a difficulty
-char Intro() 
+void PrintHangMan(int iLives)
 {
-	
-	std::cout << "Hello and Welcome to Hangman" << "\n";
-	std::cout << "Pres any key to continue..." << "\n";
+
+	if (iLives == 7)
+	{
+		std::cout << "\t\t +---+\n";
+		std::cout << "\t\t     |\n";
+		std::cout << "\t\t     | \n";
+		std::cout << "\t\t     |\n";
+		std::cout << "\t\t     | \n";
+		std::cout << "\t\t     |\n";
+		std::cout << "\t\t=========\n";
+	}
+	else if (iLives == 6)
+	{
+		std::cout << "\t\t +---+\n";
+		std::cout << "\t\t |   |\n";
+		std::cout << "\t\t     | \n";
+		std::cout << "\t\t     |\n";
+		std::cout << "\t\t     | \n";
+		std::cout << "\t\t     |\n";
+		std::cout << "\t\t=========\n";
+	}
+	else if (iLives == 5)
+	{
+		std::cout << "\t\t +---+\n";
+		std::cout << "\t\t |   |\n";
+		std::cout << "\t\t O   | \n";
+		std::cout << "\t\t     |\n";
+		std::cout << "\t\t     | \n";
+		std::cout << "\t\t     |\n";
+		std::cout << "\t\t=========\n";
+	}
+	else if (iLives == 4)
+	{
+		std::cout << "\t\t +---+\n";
+		std::cout << "\t\t |   |\n";
+		std::cout << "\t\t O   | \n";
+		std::cout << "\t\t |   |\n";
+		std::cout << "\t\t     | \n";
+		std::cout << "\t\t     |\n";
+		std::cout << "\t\t=========\n";
+	}
+	else if (iLives == 3)
+	{
+		std::cout << "\t\t +---+\n";
+		std::cout << "\t\t |   |\n";
+		std::cout << "\t\t O   | \n";
+		std::cout << "\t\t |\\  |\n";
+		std::cout << "\t\t     | \n";
+		std::cout << "\t\t     |\n";
+		std::cout << "\t\t=========\n";
+	}
+	else if (iLives == 2)
+	{
+		std::cout << "\t\t +---+\n";
+		std::cout << "\t\t |   |\n";
+		std::cout << "\t\t O   | \n";
+		std::cout << "\t\t/|\\  |\n";
+		std::cout << "\t\t     | \n";
+		std::cout << "\t\t     |\n";
+		std::cout << "\t\t=========\n";
+	}
+	else if (iLives == 1)
+	{
+		std::cout << "\t\t +---+\n";
+		std::cout << "\t\t |   |\n";
+		std::cout << "\t\t O   | \n";
+		std::cout << "\t\t/|\\  |\n";
+		std::cout << "\t\t  \\  | \n";
+		std::cout << "\t\t     |\n";
+		std::cout << "\t\t=========\n";
+	}
+	else {
+		std::cout << "\t\t +---+\n";
+		std::cout << "\t\t |   |\n";
+		std::cout << "\t\t O   | \n";
+		std::cout << "\t\t/|\\  |\n";
+		std::cout << "\t\t/ \\  | \n";
+		std::cout << "\t\t     |\n";
+		std::cout << "\t\t=========\n";
+
+	}
+}
+void ChangeTextColour(int iCoulorValue)
+{
+//0 = Black 
+//1 = Blue 
+//2 = Green 
+//3 = Aqua 
+//4 = Red 
+//5 = Purple
+//6 = Yellow 
+//7 = White 
+
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), iCoulorValue);
+}
+void ChangeConsoleWindowSize(int iXValue, int iYvalue)
+{
+	// changes the size of the consloe windows X and Y cordinates 
+	HWND console = GetConsoleWindow();
+	RECT ConsoleRect;
+	GetWindowRect(console, &ConsoleRect);
+
+	MoveWindow(console, ConsoleRect.left, ConsoleRect.top, iXValue, iYvalue, TRUE);
+
+}
+char Intro()
+{
+	system("cls");
+
+	// printing the intro mesage to the screen
+	ChangeConsoleWindowSize(400, 150);
+	ChangeTextColour(2);
+	std::cout << "==================================================";
+	ChangeTextColour(7);
+	std::cout << "\n\n	   Hello and Welcome to Hangman" << "\n";
+	std::cout << "	   Pres any key to continue..." << "\n\n";
+	ChangeTextColour(2);
+	std::cout << "==================================================";
 
 	_getch();
 	system("cls");
-	
-	std::cout << "<<<HANGMAN>>>" << "\n";
-	std::cout << "Please select your dificulty" << "\n";
-	std::cout << "1) Easy"  << "\n";
-	std::cout << "2) Medium" << "\n";
-	std::cout << "3) Hard" "\n";
 
+	//top banner 
+	ChangeConsoleWindowSize(250, 200);
+	ChangeTextColour(2);
+	std::cout << "=========";
+	ChangeTextColour(7);
+	std::cout << "<<<HANGMAN>>>";
+	ChangeTextColour(2);
+	std::cout << "========\n";
+	ChangeTextColour(7);
+
+	//menu text
+	std::cout << "\nPlease select your dificulty" << "\n";
+	std::cout << "	1) Easy" << "\n";
+	std::cout << "	2) Medium" << "\n";
+	std::cout << "	3) Hard" "\n\n";
+
+	//bottom banner 
+	ChangeTextColour(2);
+	std::cout << "=========";
+	ChangeTextColour(7);
+	std::cout << "<<<HANGMAN>>>";
+	ChangeTextColour(2);
+	std::cout << "========\n";
+	ChangeTextColour(7);
+
+	// getting the dificuly the play has selected 
 	char cDificulty = 0;
-	cDificulty = _getch();
+	do
+	{
+		cDificulty = _getch();
+	} while (cDificulty != '1' && cDificulty != '2' && cDificulty != '3');
+
 	system("cls");
 	return cDificulty;
 
 }
+std::set<char> FillAlphabet(std::set<char> sAlphabet)
+{
+	//adding letters to alpha 
+	for (int i = 97; i <= 122; i++)
+	{
+		char c = i;
+		sAlphabet.insert(i);
+	}
 
-void PrintWord(std::string sWord, std::vector<char> vPLayerGuss) 
+	return sAlphabet;
+}
+void PrintAplhabet(std::set<char> sAlphabet, std::set<char> sCorretGuess)
+{
+
+	char cLetter;
+
+	std::cout << "   ";
+	for (int i = 97; i <= 122; i++)
+	{
+		cLetter = i;
+
+		std::cout << " ";
+		if (sAlphabet.find(cLetter) != sAlphabet.end())
+		{
+			std::cout << cLetter;
+		}
+		else if (sCorretGuess.find(cLetter) != sCorretGuess.end())
+		{
+			ChangeTextColour(3);
+			std::cout << cLetter;
+			ChangeTextColour(7);
+		}
+		else
+		{
+			ChangeTextColour(4);
+			std::cout << cLetter;
+			ChangeTextColour(7);
+		}
+
+		if(i == 111)
+		{
+			std::cout << "\n       ";
+		}
+	}
+}
+char GetLetter()
+{
+	bool bIsletter = false;
+	char cLetter;
+	do
+	{
+		cLetter = _getch();
+		// making sure the char inputed is a letter 
+		if(cLetter >= 'a' && cLetter <= 'z' || cLetter >= 'A' && cLetter <= 'Z')
+		{
+			bIsletter = true;
+		}
+	} while (!bIsletter);
+	
+	return cLetter;
+}
+bool CheckDuplicateLetter(std::set<char> sListOfLetters, char cletter)
+{
+	// if the letter is in the <set>list it means the user has not picked that letter 
+	if (sListOfLetters.find(cletter) != sListOfLetters.end())
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+bool IsGuessCorrect(std::vector<char> vGuess, std::string word)
+{
+	bool bLetterInWord = false;
+	for (int i = 0; i < word.length(); i++)
+	{
+		if(vGuess.back() == word[i])
+		{
+			bLetterInWord = true;
+		}
+	}
+
+	if (bLetterInWord)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+void PrintWord(std::string sWord, std::vector<char> vPLayerGuss)
 {
 
 	//this will print out letters or '_'
-	for(int i  = 0 ; i < sWord.length() ; i++ )
+	for (int i = 0; i < sWord.length(); i++)
 	{
 		// if the user has guessed a letter and it is in the Vetor loop will print out the letter
-		if(std::count(vPLayerGuss.begin(), vPLayerGuss.end() , sWord[i]))
+		if (std::count(vPLayerGuss.begin(), vPLayerGuss.end(), sWord[i]))
 		{
 			std::cout << sWord[i];
 		}
@@ -46,95 +279,218 @@ void PrintWord(std::string sWord, std::vector<char> vPLayerGuss)
 		}
 	}
 }
-
-int WinCondition(std::string sWord, std::vector<char> vPlayerGuess, int iLives)
+void PrintGameState(int iLives, std::string sWord, std::vector<char> vPLayerGuss)
 {
+	system("cls");
 
-	//looking to see if the players latest guess is correct 
-	bool bCorrect = false;
+	//top banner 
+	ChangeConsoleWindowSize(300, 300);
+	ChangeTextColour(2);
+	std::cout << "============";
+	ChangeTextColour(7);
+	std::cout << "<<<HANGMAN>>>";
+	ChangeTextColour(2);
+	std::cout << "============\n";
+	ChangeTextColour(7);
+
+	PrintHangMan(iLives);
+
+	//guess banner 
+	ChangeTextColour(2);
+	std::cout << "=============";
+	ChangeTextColour(7);
+	std::cout << "<<<Guess>>>";
+	ChangeTextColour(2);
+	std::cout << "============\n";
+	ChangeTextColour(7);
+
+	std::cout << "\t\t";
+	PrintWord(sWord, vPLayerGuss);
+	std::cout << "\n";
+
+	//guess banner 
+	ChangeTextColour(2);
+	std::cout << "=============";
+	ChangeTextColour(7);
+	std::cout << "<<<Guess>>>";
+	ChangeTextColour(2);
+	std::cout << "============\n";
+	ChangeTextColour(7);
+
+
+
+
+}
+void PrintGameOverScreen(int iLives)
+{
+	system("cls");
+	ChangeConsoleWindowSize(300, 300);
+
+	//top banner 
+	ChangeTextColour(2);
+	std::cout << "=============";
+	ChangeTextColour(7);
+	std::cout << "<<<GAMEOVER>>>";
+	ChangeTextColour(2);
+	std::cout << "==========\n";
+	ChangeTextColour(7);
+
+	ChangeTextColour(4);
+	PrintHangMan(iLives);
+	ChangeTextColour(7);
+
+	//guess banner 
+	ChangeTextColour(2);
+	std::cout << "==========";
+	ChangeTextColour(7);
+	std::cout << "<<<Press Any Key>>>";
+	ChangeTextColour(2);
+	std::cout << "========";
+	ChangeTextColour(7);
+	_getch();
+}
+void PrintWinScreen(int iLives, std::string sWord)
+{
+	system("cls");
+	ChangeConsoleWindowSize(300, 300);
+
+	//top banner 
+	ChangeTextColour(2);
+	std::cout << "=============";
+	ChangeTextColour(7);
+	std::cout << "<<<YOU WIN>>>";
+	ChangeTextColour(2);
+	std::cout << "==========\n";
+	ChangeTextColour(7);
+
+	ChangeTextColour(3);
+	PrintHangMan(iLives);
+	std::cout << "\n\t\t" <<sWord << "\n";
+	ChangeTextColour(7);
+
+	//guess banner 
+	ChangeTextColour(2);
+	std::cout << "==========";
+	ChangeTextColour(7);
+	std::cout << "<<<Press Any Key>>>";
+	ChangeTextColour(2);
+	std::cout << "========";
+	ChangeTextColour(7);
+	_getch();
+}
+bool CheckIfPlayerHasWon(std::string sWord, std::set <char> sCorrectGuesses)
+{		
+	
+	// if all of the letter cant be found in the set player has not won 
 	for (int i = 0; i < sWord.length(); i++)
 	{
-		// if the last guess they player made is in the word bCorrect becomes true 
-		if ( vPlayerGuess.back() == sWord[i])
+
+		if(!(sCorrectGuesses.find(sWord[i]) != sCorrectGuesses.end()))
 		{
-			bCorrect = true;
+			return false;
 		}
 	}
-
-	if (!bCorrect)
-	{
-		iLives--;
-	}
-
-	std::cout << iLives << "\n";
-	return iLives;
+	return true;
 }
 
 
 int main()
 {
-
-
-	// creating the arrays that will hold the words for the game
-	std::string asEasyWords[3]{"dog", "log", "car"};
+	//collection used 
+	std::vector<char> vGuessedLetters;
+	std::set<char> sCorrectGuess;
+	std::set<char> sRemainingLetters;
+	
+	std::string asEasyWords[3]{ "dog", "log", "car" };
 	std::string asMediumWords[3]{ "animal", "window", "purple" };
 	std::string asHardWords[3]{ "razzberry", "blackjack", "jukebox" };
 
-	// getting a random number between one and 3 to paick a word
-	srand(time(NULL));
-	int iRandomNumber = rand() % 3;
 	
 
-	//number of lives the player will have 
-	 int iLives = 5;
+	//testing word 
+	std::string HiddenWord;
 
-	//crearting the vector that will hold the players guess
-	std::vector<char> vcPlayersGuesses;
+	// set up variables 
+	int iLives = 7;
+	bool bHasPlayerWon = false;
+	
 
-	// plaing the intro and letting the user pick there difficulty 
-	char cDificulty = Intro();
 
-
-	if(cDificulty == '1')
+	
+	//Game Loop
+	do
 	{
-		std::string sHiddenWord = asEasyWords[iRandomNumber];
+		// getting a random number between one and 3 to paick a word
+		srand(time(NULL));
+		int iRandomNumber = rand() % 3;
+
+		char cDifficulty = Intro();
+		sRemainingLetters = FillAlphabet(sRemainingLetters);
 		
-		do
+		//picking the word
+		switch (cDifficulty)
 		{
-			std::cout << "Guess : " << "\n";
-			vcPlayersGuesses.push_back( _getch());
-			PrintWord(sHiddenWord, vcPlayersGuesses);
-			iLives = WinCondition(sHiddenWord, vcPlayersGuesses, iLives);
-
-		} while (true);
-
-	}
-	else if(cDificulty == '2')
-	{
-		std::string sHiddenWord = asMediumWords[iRandomNumber];
-
-		do
-		{
-			std::cout << "Guess : " << "\n";
-			vcPlayersGuesses.push_back(_getch());
-			PrintWord(sHiddenWord, vcPlayersGuesses);
-			iLives = WinCondition(sHiddenWord, vcPlayersGuesses, iLives);
-
-		} while (true);
-	}
-	else if(cDificulty == '3')
-	{
-		std::string sHiddenWord = asHardWords[iRandomNumber];
+		case '1':
+			HiddenWord = asEasyWords[iRandomNumber];
+			break;
+		case '2':
+			HiddenWord = asMediumWords[iRandomNumber];
+			break;
+		case '3':
+			HiddenWord = asHardWords[iRandomNumber];
+			break;
+		}
 
 		do
 		{
-			std::cout << "Guess : " << "\n";
-			vcPlayersGuesses.push_back(_getch());
-			PrintWord(sHiddenWord, vcPlayersGuesses);
-			iLives = WinCondition(sHiddenWord, vcPlayersGuesses, iLives);
+			PrintGameState(iLives, HiddenWord, vGuessedLetters);
+			std::cout << "\n\n";
+			PrintAplhabet(sRemainingLetters, sCorrectGuess);
+			// getting letter from user and removing it form Remaining Letters
+			bool bDuplicateLetter;
+			char cLetter;
+			do
+			{
+				cLetter = GetLetter();
+				bDuplicateLetter = CheckDuplicateLetter(sRemainingLetters, cLetter);
 
-		} while (true);
-	}
+			} while (bDuplicateLetter);
 
+			vGuessedLetters.push_back(cLetter);
+			sRemainingLetters.erase(vGuessedLetters.back());
+
+			//cheking to see if the user guessed a letter if they guessed worng they lose a life 
+			bool bCorrectGuess = IsGuessCorrect(vGuessedLetters, HiddenWord);
+			if (bCorrectGuess)
+			{
+				sCorrectGuess.insert(cLetter);
+			}
+			else
+			{
+				iLives--;
+			}
+
+			//cheking if player has won the game
+			bHasPlayerWon = CheckIfPlayerHasWon(HiddenWord, sCorrectGuess);
+
+
+		} while (iLives > 0 && !bHasPlayerWon);
+
+		if (bHasPlayerWon)
+		{
+			PrintWinScreen(iLives, HiddenWord);
+		}
+		else
+		{
+			PrintGameOverScreen(iLives);
+		}
+		
+		//resteting for next game
+		sRemainingLetters.clear();
+		sCorrectGuess.clear();
+		vGuessedLetters.clear();
+		iLives = 7;
+
+	} while (true);
 	
 }
